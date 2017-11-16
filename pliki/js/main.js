f00aa55291de78c573766e8de1a07af875640591
alternatywny <33729621+alternatywny@users.noreply.github.com>
2017-11-16 18:42:50 +0100

    vet website
    First page veterinary office, html, css bootstrap, RWD
    There is no full content

---
+++ pliki/js/main.js
@@ -0,0 +1,25 @@
+$(function() {
+	
+	//cache the window object
+	var $window = $(window);
+	
+	//parallax background effect
+	$('section[data-type="background"]').each(function() {
+		var $bgobj = $(this); //assigning the object
+		
+		$window.scroll(function() {
+			
+			//scroll the background at var speed
+			//the yPos is negative value because we're scrolling it UP!
+			var yPos = -($window.scrollTop() / $bgobj.data('speed'));
+			
+			//Put together our final background position
+			var coords = '30% '+ yPos + 'px';
+			
+			//Move the background$bgobj.css
+			$bgobj.css({ backgroundPosition: coords });
+		})
+	})
+	
+	
+});
