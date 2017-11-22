ad8de90ef2d5be8d1907af0a110ff58a6da7119e
alternatywny <33729621+alternatywny@users.noreply.github.com>
2017-11-22 17:06:03 +0100

    Add files via upload

--- pliki/js/main.js
+++ pliki/js/main.js
@@ -1,25 +1,31 @@
+
+
 $(function() {
-	
-	//cache the window object
-	var $window = $(window);
-	
-	//parallax background effect
-	$('section[data-type="background"]').each(function() {
-		var $bgobj = $(this); //assigning the object
+	if($(window).width() > 768){
 		
-		$window.scroll(function() {
+		//cache the window object
+		var $window = $(window);
+		
+		//parallax background effect
+		$('section[data-type="background"]').each(function() {
 			
-			//scroll the background at var speed
-			//the yPos is negative value because we're scrolling it UP!
-			var yPos = -($window.scrollTop() / $bgobj.data('speed'));
+			var $bgobj = $(this); //assigning the object
 			
-			//Put together our final background position
-			var coords = '50% '+ yPos + 'px';
 			
-			//Move the background$bgobj.css
-			$bgobj.css({ backgroundPosition: coords });
-		})
-	})
-	
-	
+				$window.scroll(function() {
+					
+					//scroll the background at var speed
+					//the yPos is negative value because we're scrolling it UP!
+					var yPos = -($window.scrollTop() / $bgobj.data('speed'));
+					
+					//Put together our final background position
+					var coords = '50% '+ yPos + 'px';
+					
+					//Move the background$bgobj.css
+					$bgobj.css({ backgroundPosition: coords });
+				})
+			
+		})	
+	}
 });
+
